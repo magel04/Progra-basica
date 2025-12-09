4509848edec14621461361d3ebb065c11b80119e
magel04 <mariangel.jimenez2020@gmail.com>
2025-12-08 22:53:46 -0600

    Add files via upload

---
+++ Caso Evaluado II.py
@@ -0,0 +1,31 @@
+"Caso Evaluado 2"
+
+def Calculosemanal(ventas): #funcion para calcular el monto semanal de cada vendedor
+    return sum 
+
+def Ventamayor (ventas): #funcion para derterminar el vendedor mayor
+    return max 
+dias=["lunes","martes","miercoles","jueves","viernes"]
+
+
+numerovendedores=int(input("Cuantos vendedores desea ingresar"))
+
+
+for i in range(numerovendedores):
+    vendedor=input("Coloque el nombre del vendedor")
+    lunes=int(input("Ingrese el monto del dia lunes"))
+    martes=int(input("Ingrese el monto del dia martes"))
+    miercoles=int(input("Ingrese el monto del dia miercoles"))
+    jueves=int(input("Ingrese el monto del dia jueves"))
+    viernes=int(input("Ingrese el monto del dia lunes"))
+    
+        
+for dia in dias:
+    ventas=[]
+    
+        
+
+Calculosemanal(ventas)
+Ventamayor(ventas)
+        
+    
